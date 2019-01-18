f500107cea3104f01261b8354e23c62aa27bbe09
Julio <julio.olvr@gmail.com>
2019-01-17 23:49:01 -0300

    Boton de start mal alineado porque el MarginContainer de mierda no me da bola

--- screens/MainMenu.gd
+++ screens/MainMenu.gd
@@ -5,6 +5,5 @@ var main_scene = preload("res://Level.tscn")
 func start_game():
 	get_tree().change_scene_to(main_scene)
 
-func _on_StartButton_gui_input(ev):
-	if ev.is_action('ui_click'):
-		start_game()
+func _on_StartButton_pressed():
+	start_game()
