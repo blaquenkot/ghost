192c79a3a5a13285af6df052bcecbd5f8a9a3fb5
Julio <julio.olvr@gmail.com>
2019-01-18 00:01:15 -0300

    Mejor pantalla de fin

--- screens/EndScreen.gd
+++ screens/EndScreen.gd
@@ -2,9 +2,8 @@ extends CanvasLayer
 
 var main_scene = preload("res://Level.tscn")
 
-func _on_Label_gui_input(ev):
-	if ev.is_action('ui_click'):
-		restart_game()
-
 func restart_game():
 	get_tree().change_scene_to(main_scene)
+
+func _on_RestartButton_pressed():
+	restart_game()
