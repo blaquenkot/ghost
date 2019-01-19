e0e565b2e873732aff49d6d1d5864ffd602ea872
Julio <julio.olvr@gmail.com>
2019-01-19 16:10:32 -0300

    Dead animation color per enemy

---
+++ enemy/DeadAnimation.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+export(Color) var color = Color(1, 0, 1)
+export(int) var size = 2
+export(int) var width = 10
+
+func init(color, size, width):
+	self.color = color
+	self.size = size
+	self.width = width
+
+func _ready():
+	for i in range(20):
+		var particle = create_particle()
+		particle.apply_impulse(Vector2(0, 0), Vector2(randf() * 100 - 50, randi() % 10 - 20))
+		add_child(particle)
+
+func create_particle():
+	var body = RigidBody2D.new()
+
+	body.set_collision_layer_bit(0, true)
+	body.set_collision_layer_bit(5, true)
+	body.set_collision_mask_bit(0, true)
+	body.set_gravity_scale(3)
+	
+	var collider = CollisionShape2D.new()
+	collider.position = Vector2(size / 2, size / 2)
+	var shape = RectangleShape2D.new()
+	shape.set_extents(Vector2(size / 2, size / 2))
+	collider.set_shape(shape)
+	body.add_child(collider)
+	
+	var geometry = Polygon2D.new()
+	var points = PoolVector2Array()
+	points.push_back(Vector2(0, 0))
+	points.push_back(Vector2(size, 0))
+	points.push_back(Vector2(size, size))
+	points.push_back(Vector2(0, size))
+	geometry.set_polygon(points)
+	geometry.set_color(color)
+	body.add_child(geometry)
+	
+	return body
--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -1,5 +1,10 @@
 extends KinematicBody2D
 
+export(Color, RGBA) var dead_animation_color
+export(int) var dead_particles_size = 2 
+
+var DeadAnimation = preload("res://enemy/DeadAnimation.tscn")
+
 onready var parent = get_parent()
 
 export (int) var speed = 75
@@ -15,5 +20,24 @@ func _on_Area2D_body_entered(body):
 		body.take_damage((position - body.position).normalized())
 
 func attacked():
+	die()
+	
+func die():
 	global.enemyKilledSFXPlayer.play()
+	disappear()
+	show_death_animation()
+	$DieTimer.start()
+	
+func show_death_animation():
+	var animation = DeadAnimation.instance()
+	animation.init(dead_animation_color, dead_particles_size, $Sprite.region_rect.size.y)
+	speed = 0
+	add_child(animation)
+	
+func disappear():
+	$Sprite.hide()
+	$CollisionShape2D.disabled = true
+	$Area2D.monitoring = false
+
+func _on_DieTimer_timeout():
 	queue_free()
